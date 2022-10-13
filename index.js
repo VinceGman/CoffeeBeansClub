fd486e5cd09bf0a28edc93891603d03d3d29325a
Vince Gonzalez <seventito777@gmail.com>
2022-10-12 21:52:21 -0700

    react boilerplate

--- index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-    res.send('This is the official Beans Discord Website! Woo!');
-});
-
-app.listen(process.env.PORT || 80);
